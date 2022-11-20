1f3b075bde50851ce8bcb352e0950780aa0faac1
Oz Ramos <heyozramos@gmail.com>
2022-11-19 21:34:16 -0800

    #0 Improves styles for calendars, boards, checkbox

--- src/styles/main.css.js
+++ src/styles/main.css.js
@@ -31,6 +31,9 @@ div.notion-topbar-mobile > div:nth-child(1n).toggle-mode { display: block !impor
   color: #fff;
 }
 
+body a {
+  color: #fad000;
+}
 
 .notion-transclusion_container-block,
 .notion-callout-block {
@@ -45,12 +48,30 @@ div.notion-topbar-mobile > div:nth-child(1n).toggle-mode { display: block !impor
   color: #fff !important;
 }
 
+/* todo */
+.notion-to_do-block * {
+  color: #fff !important;
+}
+body .notion-reminder, .notion-text-mention-token *,
+.notion-bulleted_list-block .pseudoBefore:before {
+  color: #ff628c !important;
+}
+a > .notion-text-mention-token *,
+a > .notion-page-mention-token__title,
+a.notion-link-token > span {
+  color: #fad000 !important;
+  font-weight: 900 !important;
+  opacity: 1 !important;
+}
+.notion-to_do-block [style*="text-decoration: line-through"] {
+  text-decoration: 2px line-through #ff628c !important;
+}
+
 
 
 /* Gallery */
-.notion-page-block.notion-collection-item a div,
-.notion-collection_view-block + div {
-  background: #4a468b
+.notion-collection-item > a {
+  background: #4a468b !important;
 }
 
 .notion-collection_view-block div[style*="border-bottom: 2px solid rgb("] {
@@ -61,8 +82,33 @@ div.notion-topbar-mobile > div:nth-child(1n).toggle-mode { display: block !impor
   box-shadow: none !important;
 }
 
-.notion-collection_view-block * {
+.notion-collection_view-block *:not([class*=status][class$=Group]):not([class*=status][class$=Group] ~ *) {
   fill: #fff !important;
   color: #fff !important;
+}
+
+/* Boards */
+.notion-board-view > .notion-collection_view-block > div {
+  background: none !important;
+  fill: none !important
+}
+
+.notion-board-view > .notion-collection_view-block > div:first-child div:first-child {
+  background: none !important;
+}
+
+/* Calendar */
+.notion-calendar-header-days,
+.notion-calendar-view > .notion-collection_view-block > div:first-child div:first-child {
+  background: #4a468b !important;
+}
+.notion-calendar-view .notion-calendar-header-days * {
+  color: #9effff !important;
+  font-weight: bold !important;
+}
+
+.notion-calendar-view > .notion-collection_view-block > *,
+.notion-collection-view-body .notion-calendar-view .notion-collection_view-block {
+  background: none !important;
 }
 `
